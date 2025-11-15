6f2ba470bdb16caeba94f61edeb982b8511103ca
SmokingA1 <denisshokolad1221@gmail.com>
2025-11-15 18:12:09 +0100

    Connection to database and creating sample of FastApi

---
+++ backend/app/core/config.py
@@ -0,0 +1,26 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings): 
+    model_config = SettingsConfigDict(
+        env_file="../.env", 
+        env_ignore_empty=True,
+        extra="ignore"
+    )
+
+    PROJECT_NAME: str
+
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+    DATABASE_HOST: str = "localhost"
+    DATABASE_PORT: int = 5432
+    DATABASE_DB_NAME: str
+
+    @property
+    def async_DB_URL(self):
+        return f"postgresql+asyncpg://{self.DATABASE_USER}:{self.DATABASE_PASSWORD}@{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_DB_NAME}"
+    
+
+settings = Settings()
+
+
+
---
+++ backend/app/core/database.py
@@ -0,0 +1,47 @@
+from datetime import datetime, timezone
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.types import TypeDecorator, DateTime
+
+from app.core.config import settings
+
+class Base(DeclarativeBase):
+    pass
+
+
+class TZDateTime(TypeDecorator):
+    impl = DateTime
+    cache_ok = True
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            if not value.tzinfo:
+                raise TypeError("tzinfo is required")
+            value = value.astimezone(timezone.utc).replace(tzinfo=None)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = value.replace(tzinfo=timezone.utc)
+        return value
+
+
+class TimestampMixin:
+    created_at: Mapped[datetime] = mapped_column(TZDateTime(), default=datetime.now(timezone.utc))
+    updated_at: Mapped[datetime] = mapped_column(TZDateTime(), default=datetime.now(timezone.utc) ,onupdate=datetime.now(timezone.utc))
+
+
+
+async_engine = create_async_engine(settings.async_DB_URL, echo=True)
+async_session = async_sessionmaker(
+    autoflush=False,
+    autocommit=False,
+    class_=AsyncSession,
+    bind=async_engine
+)
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as async_s:
+        yield async_s
